124e410530be0e3c61764221e30516e5b817c8da
Nelson <nelson.atiang@student.moringaschool.com>
2025-06-30 17:17:24 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -16,11 +16,55 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.all()
+    return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = db.session.get(Message, id)
+
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    data = request.get_json()
+    if "body" in data:
+        message.body = data["body"]
+
+    db.session.commit()
+
+    return jsonify(message.to_dict()), 200
+
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+
+    new_message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(new_message.to_dict()), 201
+
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id)
+
+
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    db.session.delete(message)
+    db.session.commit()
+
+    return '', 204
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
